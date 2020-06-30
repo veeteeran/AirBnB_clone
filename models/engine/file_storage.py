53a2a1e5691addd5de16cc1b746216d3370f85b2
Viet Tran <vietnamtran@gmail.com>
2020-06-30 02:22:59 +0000

    MAke dunder obj a dict of objs

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,5 +1,5 @@
-#!/usr/bin/ptyhon3
-"""The first firle storage engine of HolBnB"""
+#!/usr/bin/python3
+"""The first file storage engine of HolBnB"""
 import json
 import os
 
@@ -21,18 +21,28 @@ class FileStorage:
         Parameters: Takes in an object and adds it to the dictionary __objects
         the key is the obj class+ obj id, the value is the dict of the obj
         """
-        return self.__objects.update({str((type(obj).__name__)+'.'+(obj.id)): obj.to_dict()})
+        return self.__objects.update({str((type(obj).__name__)+'.'+(obj.id)): obj})
 
     def save(self):
         """ serializes __objects to the JSON file (path: __file_path)"""
         filename = self.__file_path + 'file.json'
         with open(filename, 'w') as output:
-            json.dump(self.__objects, output)
+            new_dict = {}
+            for k, v in self.__objects.items():
+                new_dict.update({k: v.to_dict()})
+
+            json.dump(new_dict, output)
 
     def reload(self):
         """deserializes the JSON file to __objects if __file_path exists"""
+        from models.base_model import BaseModel
         try:
             with open(self.__file_path + 'file.json') as saved_data:
-                self.__objects = json.load(saved_data)
+                new_dict = json.load(saved_data)
+                for k, v in new_dict.items():
+                    new_obj = BaseModel(v)
+                    key = str((type(new_obj).__name__)+'.'+(new_obj.id))
+                    if key not in self.__objects.keys():
+                        self.__objects.update({key: new_obj})
         except:
             pass
