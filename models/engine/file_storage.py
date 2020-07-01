b3c26c3599c8071f4289d55a35d6014aa0faded0
timmcmackenjr@gmail.com <timmcmackenjr@gmail.com>
2020-07-01 14:01:39 +0000

    perserving changes in filestorage

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -8,7 +8,7 @@ class FileStorage:
     """ A class to serialize and deserialize JSON and Python Dicts """
 
     __objects = {}
-    __file_path = os.getcwd() + "/models/engine/json/"
+    __file_path = "file.json"
 
     def all(self):
         """returns the dictionary __objects"""
@@ -21,11 +21,12 @@ class FileStorage:
         the key is the obj class+ obj id, the value is the dict of the obj
         """
         key = str((type(obj).__name__) + '.' + (obj.id))
+        print("The Key in New: {}".format(key))
         return self.__objects.update({key: obj})
 
     def save(self):
         """ serializes __objects to the JSON file (path: __file_path)"""
-        filename = self.__file_path + 'file.json'
+        filename = self.__file_path
         with open(filename, 'w') as output:
             new_dict = {}
             for k, v in self.__objects.items():
@@ -36,12 +37,27 @@ class FileStorage:
     def reload(self):
         """deserializes the JSON file to __objects if __file_path exists"""
         from models.base_model import BaseModel
+        from models.user import User
+        from models.place import Place
+        from models.state import State
+        from models.city import City
+        from models.amenity import Amenity
+        from models.review import Review
+
+        classes = {"BaseModel": BaseModel, "User": User,
+                   "Place": Place, "State": State,
+                   "City": City, "Amenity": Amenity,
+                   "Review": Review}
         try:
-            with open(self.__file_path + 'file.json') as saved_data:
+            with open(self.__file_path) as saved_data:
                 new_dict = json.load(saved_data)
                 for k, v in new_dict.items():
-                    new_obj = BaseModel(**v)
-                    key = str((type(new_obj).__name__) + '.' + (new_obj.id))
+                    print("IN NEWDICT K: {} V: {}".format(k, v))
+                    if new_dict[k]['__class__'] in classes.keys():
+                        new_obj = classes[new_dict[k].__class__](**v)
+                        key = str((type(new_obj).__name__) + '.' + (new_obj.id))
+                    print("The Key in Reload: {} ".format(key))
+
                     self.__objects.update({key: new_obj})
         except:
             pass
