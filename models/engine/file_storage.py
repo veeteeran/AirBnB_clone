4ab27ef05feb9447c9ca5442efb8271566f855fc
timmcmackenjr@gmail.com <timmcmackenjr@gmail.com>
2020-07-01 19:12:22 +0000

    Deletes all the __pycache__ files and adds the imports and the classes dictionary to reload.

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -36,6 +36,17 @@ class FileStorage:
     def reload(self):
         """deserializes the JSON file to __objects if __file_path exists"""
         from models.base_model import BaseModel
+        from models.user import User
+        from models.place import Place
+        from models.state import State
+        from models.city import City
+        from models.amenity import Amenity
+        from models.review import Review
+
+        classes = {"BaseModel": BaseModel, "User": User,
+                   "Place": Place, "State": State, "City": City,
+                   "Amenity": Amenity, "Review": Review}
+
         try:
             with open(self.__file_path) as saved_data:
                 new_dict = json.load(saved_data)
